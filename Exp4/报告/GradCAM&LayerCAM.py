1e892d1c744d5e89048c63594b10ac6ed3bff00c
harsamker <1421068609@qq.com>
2023-12-30 14:14:59 +0800

    Fin Exp4

--- Exp4/报告/GradCAM&LayerCAM.py
+++ Exp4/报告/GradCAM&LayerCAM.py
@@ -5,16 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-# 预处理图像的函数
-def preprocess_image(img_path):
-    transform = transforms.Compose(
-        [
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-    image = Image.open(img_path)
+def preprocess_img(img_path):
+    transform = transforms.Compose([
+        transforms.ToTensor(), 
+    ])
+    image = Image.open(img_path).convert('RGB')
     image = transform(image).unsqueeze(0)
     return image
 
@@ -25,11 +20,8 @@ def get_grad_hook(module, input, output):
     target_layer.gradients = output[0]
     
 def layercam(image_path, model, target_layer, target_class):
-
-    rgb_img = cv2.imread(image_path, 1)[:, :, ::-1]
-    original_image = np.copy(rgb_img)
-    rgb_img = np.float32(rgb_img) / 255
-    input_tensor = preprocess_image(image_path)
+    original_image = cv2.imread(image_path, 1)[:, :, ::-1]
+    input_tensor = preprocess_img(image_path).to(device)
     # 前向传播
     model.zero_grad()
     output = model(input_tensor)
@@ -39,6 +31,7 @@ def layercam(image_path, model, target_layer, target_class):
     gradients = target_layer.gradients 
     activations = target_layer.activations 
     grad_activations = gradients * activations
+    
     cam = torch.relu(torch.sum(grad_activations, dim=1)).squeeze(0)
     cam = cam.cpu().detach().numpy()
     cam = cv2.resize(cam, (original_image.shape[1], original_image.shape[0]))
@@ -46,39 +39,38 @@ def layercam(image_path, model, target_layer, target_class):
     cam_normalized = cam / np.max(cam)
     heatmap_colored = cv2.applyColorMap(np.uint8(255 * cam_normalized), cv2.COLORMAP_JET)
     heatmap_colored = cv2.cvtColor(heatmap_colored, cv2.COLOR_BGR2RGB)
+    # 叠加热图到原始图像
     img = heatmap_colored * 0.4 + original_image * 0.6
     img = np.clip(img, 0, 255).astype(np.uint8)
+    
     return img
-
-
-
 def gradcam(image_path, model, target_layer, target_class):
-    rgb_img = cv2.imread(image_path, 1)[:, :, ::-1]
-    original_image = np.copy(rgb_img)
-    rgb_img = np.float32(rgb_img) / 255
-    input_tensor = preprocess_image(image_path)
-    # 前向传播
+    input_tensor = preprocess_img(image_path).to(device)
     model.zero_grad()
     output = model(input_tensor)
     class_activation = output[:, target_class]
     # 反向传播
     class_activation.backward()
+    # 获取目标层的激活和梯度
     gradients = target_layer.gradients
-    pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
     activations = target_layer.activations
+    pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
+    # 权重化激活图
     for i in range(activations.shape[1]):
         activations[:, i, :, :] *= pooled_gradients[i]
     heatmap = torch.mean(activations, dim=1).squeeze()
     heatmap = np.maximum(heatmap.cpu().detach().numpy(), 0)
     heatmap /= np.max(heatmap)
-    heatmap = cv2.resize(heatmap, (original_image.shape[1], original_image.shape[0]))
-    heatmap_colored = cv2.applyColorMap(np.uint8(255 * heatmap), cv2.COLORMAP_JET)
-    heatmap_colored = cv2.cvtColor(heatmap_colored, cv2.COLOR_BGR2RGB)
-    img = heatmap_colored * 0.4 + original_image * 0.6
-    img = np.clip(img, 0, 255).astype(np.uint8)
-    return img
+    heatmap = cv2.resize(heatmap, (224, 224))
+    heatmap = cv2.applyColorMap(np.uint8(255 * (1.0 - heatmap)), cv2.COLORMAP_JET)
+    original_image = cv2.imread(image_path, 1)[:, :, ::-1]
+    original_image = cv2.resize(original_image, (224, 224))
+    # 叠加热图到原始图像
+    superimposed_img = heatmap * 0.4 + original_image * 0.6
+    superimposed_img = np.clip(superimposed_img, 0, 255).astype(np.uint8)
 
-# 加载模型
+    return superimposed_img
+
 model = torch.load("Exp4/torch_alex.pth")
 model.eval()
 target_layer = model.features[10] 
@@ -86,15 +78,15 @@ target_layer.activations = None
 target_layer.gradients = None
 target_layer.register_forward_hook(get_activate_hook)
 target_layer.register_backward_hook(get_grad_hook)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
 image_paths = ["Exp4/data4/dog.jpg", "Exp4/data4/cat.jpg", "Exp4/data4/both.jpg"]
 target_class_list = [0, 1]
 type_map = {0: "Cat", 1: "Dog"}
-
-
 num_rows = len(image_paths)
 num_cols = len(target_class_list) * 2
 fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 8))
-
 for i, img_path in enumerate(image_paths):
     for j, target_class in enumerate(target_class_list):
         grad_cam_image = gradcam(img_path, model, target_layer, target_class)
--- Exp4/报告/OutputOfEachConvChannels.py
+++ Exp4/报告/OutputOfEachConvChannels.py
@@ -4,21 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-
 def preprocess_image(img_path):
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-        ]
-    )
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
     image = Image.open(img_path)
     image = transform(image).unsqueeze(0)
     return image
 
-
 def get_activations_hook(module, input, output):
     target_layer.activations = output
-
 
 def visualize_feature_maps(image_path, model, target_layer):
     input_tensor = preprocess_image(image_path)
@@ -32,12 +27,11 @@ def visualize_feature_maps(image_path, model, target_layer):
         for j in range(num_cols):
             idx = i * num_cols + j
             ax = axes[i, j]
-            ax.imshow(feature_maps[idx], cmap="gray")
-            ax.axis("off")
-    plt.suptitle(image_path + " each Conv Channels Outputs")
+            ax.imshow(feature_maps[idx], cmap='gray')
+            ax.axis('off')
+    plt.suptitle(image_path+" each Conv2D Channels Outputs")
     plt.subplots_adjust(wspace=0.1, hspace=0.01)
     plt.show()
-
 
 model = torch.load("Exp4/torch_alex.pth")
 model.eval()
